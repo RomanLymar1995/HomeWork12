b4d9066f32fbc1b738b4fec6d98e9be68d93701f
RomanLymar1995 <imypokoritel@gmail.com>
2023-08-13 14:02:44 +0300

    Commit 2

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,6 +5,7 @@ public class FizzBuzz {
     public static void main(String[] args) {
         int n = 15;
         BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+        Object lock = new Object();
 
         Thread threadA = new Thread(() -> {
             for (int i = 1; i <= n; i++) {
@@ -43,12 +44,18 @@ public class FizzBuzz {
         });
 
         Thread threadD = new Thread(() -> {
-            for (int i = 1; i <= n; i++) {
-                try {
-                    String output = queue.take();
-                    System.out.println(output.isEmpty() ? i : output);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            synchronized (lock) {
+                for (int i = 1; i <= n; i++) {
+                    if (i % 3 != 0 && i % 5 != 0) {
+                        System.out.println(i);
+                    } else {
+                        try {
+                            String output = queue.take();
+                            System.out.println(output);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
         });
